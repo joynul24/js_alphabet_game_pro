730df90d8e0e110f22d56a321d349c4d6e211dbd
joynul24 <joynula919@gmail.com>
2024-08-25 17:53:20 +0600

    finished the alphabet clash game

--- script/alphabet.js
+++ script/alphabet.js
@@ -1,12 +1,17 @@
 function play() {
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
 
+    
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+    
     continueGame();
 }
 
 
-function continueGame(){
+function continueGame() {
     const alphabet = getRandomAlphabet();
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -15,20 +20,45 @@ function continueGame(){
     setBackgroundColorById(alphabet);
 }
 
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
 
-document.addEventListener('keyup', function handleKeyboardUpEvent(event){
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+}
+
+
+document.addEventListener('keyup', function handleKeyboardUpEvent(event) {
     // console.log(event.key);
     const playerPressed = event.key;
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
 
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
-    
-    if(playerPressed === expectedAlphabet){
-        console.log('yes i am same')
+
+    if (playerPressed === expectedAlphabet) {
+        const currentScore = getTextElementValueById('current-score');
+        const updateScore = currentScore + 1;
+        setTextElementValueById('current-score', updateScore);
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
     }
 
     else {
-        console.log('sorry bro no match')
+        const currentLife = getTextElementValueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife);
+
+        if (updateLife === 0) {
+            gameOver();
+        }
     }
 })
