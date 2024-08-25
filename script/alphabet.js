7411761507af2beb9d79fba5fb3dff81f9eb834d
joynul24 <joynula919@gmail.com>
2024-08-25 13:00:15 +0600

    finished the half section

--- script/alphabet.js
+++ script/alphabet.js
@@ -1,1 +1,34 @@
-console.log('js one')
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+
+    continueGame();
+}
+
+
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
+
+document.addEventListener('keyup', function handleKeyboardUpEvent(event){
+    // console.log(event.key);
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    
+    if(playerPressed === expectedAlphabet){
+        console.log('yes i am same')
+    }
+
+    else {
+        console.log('sorry bro no match')
+    }
+})
